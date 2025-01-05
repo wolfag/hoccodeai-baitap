aef5f39d1562a9d4ba5a6ea093901948d660e606
wolfag <wolfag91@gmail.com>
2025-01-05 11:44:35 +0700

    exercise 2 (4)

--- baitap-submit/tainguyen/02-llm-api-params/llm-api.py
+++ baitap-submit/tainguyen/02-llm-api-params/llm-api.py
@@ -3,6 +3,7 @@ import requests
 from openai import OpenAI
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 
@@ -13,6 +14,11 @@ client = OpenAI(
     api_key=os.getenv('API_KEY'),
 )
 
+def require_user_input(message):
+    user_input = ""
+    while(user_input == ""):
+        user_input = input(message)
+    return user_input
 
 
 def get_content(url):
@@ -35,7 +41,8 @@ def get_content(url):
 def exercise12():
     messages = []
     while(True):
-        question = input("Enter your question: ")
+        question = require_user_input("Please enter your question: ")
+
         if question == "0":
             break
 
@@ -54,9 +61,10 @@ def exercise12():
 def exercise3():
     want = "Summarize the following content"
     while(True):
-        url = input("Paste your url:")
+        url = require_user_input("Paste your url:")
         if url == "0":
             break
+
         content = get_content(url)
 
         if content is None:
@@ -76,8 +84,54 @@ def exercise3():
         for chunk in stream:
             print(chunk.choices[0].delta.content or "", end="")
 
+def translate(text, source_lang, target_lang):
+    want = "You are a translator with 10 years experiences in tech domain. Translate the following content from "+source_lang+" into " + target_lang + ". The translated content should be keep the same meaning and style as the original content."
+    question = " ".join([want,":",text])
+    messages = [{"role":"user","content":question}]
+
+    chat = client.chat.completions.create(
+        messages=messages,
+        model="gemma2-9b-it",
+    )
+
+    return chat.choices[0].message.content
+
 def exercise4():
-    print("doing")
+    chunk_size = 1024
+    default_output_file = os.getenv('OUTPUT_FILE_TRANSLATED')
+    default_source_lang = "English"
+    default_target_lang = "Vietnamese"
+
+    file_path = require_user_input("Enter the path to the file: ")
+    source_lang = input("Enter the source language (default is "+default_source_lang+"): ")
+    target_lang = input("Enter the target language (default is "+default_target_lang+"): ")
+    output_file = input("Enter the output file path (default is "+default_output_file+"): ")
+
+    if source_lang == "":
+        source_lang = default_source_lang
+
+    if target_lang == "":
+        target_lang = default_target_lang
+
+    if output_file == "":
+        output_file = default_output_file
+
+    if file_path == "0":
+        return
+    
+
+    with open(file_path, "r") as infile, open(output_file,"w") as outfile: 
+        while True:
+            chunk = infile.read(chunk_size)
+            if not chunk:
+                print("=>Let check '"+output_file+"' for the translated content")
+                break
+
+            sys.stdout.write("=")
+            sys.stdout.flush()
+            outfile.write(translate(chunk, source_lang, target_lang))
+
+
 def exercise5():
     print("doing")
 
@@ -87,7 +141,9 @@ def main():
 
         print("12. Ask me a question with memory")
         print("3. Summarize a webpage")
+        print("4. Translate a file")
         print("0. Show menu")
+        print("e. Exit")
 
         exercise = input("Please choose the exercise you want to do: ")
 
@@ -99,6 +155,8 @@ def main():
             exercise4()
         elif exercise == "5":
             exercise5()
+        elif exercise == "e":
+            break
         else:
             print("Invalid exercise number")
 
